aee1cc4ec62e940d2de3b989e73470ded67838e6
elli56 <111000010110001001e@gmail.com>
2021-04-24 14:30:52 +0300

    Create new class Node and class LinkedList

--- app.py
+++ app.py
@@ -1,1 +1,92 @@
-print("Hello, world!")
+''' queue '''
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.__next = None
+
+    def set_next(self, node):
+        if isinstance(node, Node) or node is None:
+            self.__next = node
+        else:
+            raise TypeError("The node must be a Node child or None")
+        
+    def get_next(self):
+        return self.__next
+    
+    def print_details(self):
+        print f"node: {self.data}"
+    
+
+
+class LinkedList:
+    def __init__(self):
+        self.__root = None
+    
+    def get_root(self):
+        return self.__root
+
+    def add_to_list(self, node):
+        if self.__root:
+            self.__root.set_next(node)
+        else:
+            self.__root = node
+
+    def print_list(self):
+        marker = self.__root
+        while marker:
+            marker.print_details()
+            marker = marker.get_next()
+
+    def find(self, data):
+        marker = self.__root
+        while marker:
+            if marker.data == data:
+                return marker
+            marker = marker.get_next()
+        raise LookupError("There are No element with such data!")
+
+    def remove_first(self):
+        if self.__root:
+            head = self.__root
+            self.__root = self.__root.get_next()
+            return head
+        raise Exception("The list is empty.")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
